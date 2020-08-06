33899ccc83d49e821e5d667961371f3d30dc9558
Sinclair Liang <sinclairliang@gmail.com>
2020-08-06 17:39:37 -0500

    worked on the back end

--- src/main/java/com/evan/wj/controller/LibraryController.java
+++ src/main/java/com/evan/wj/controller/LibraryController.java
@@ -3,13 +3,11 @@ package com.evan.wj.controller;
 import com.evan.wj.pojo.Book;
 import com.evan.wj.service.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@RestController
 public class LibraryController {
     @Autowired
     BookService bookService;
