1565fa577ca494d3dc4a58577b511a1745f38278
Sinclair Liang <sinclairliang@gmail.com>
2020-08-12 23:24:31 -0500

    fixed length

--- schema.sql
+++ schema.sql
@@ -2,26 +2,31 @@
 -- Table structure for book
 -- ----------------------------
 DROP TABLE IF EXISTS `book`;
-CREATE TABLE `book` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `cover` varchar(255) DEFAULT '',
-  `title` varchar(255) NOT NULL DEFAULT '',
-  `author` varchar(255) DEFAULT '',
-  `date` varchar(20) DEFAULT '',
-  `press` varchar(255) DEFAULT '',
-  `abs` varchar(255) DEFAULT NULL,
-  `cid` int(11) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `fk_book_category_on_cid` (`cid`),
-  CONSTRAINT `fk_book_category_on_cid` FOREIGN KEY (`cid`) REFERENCES `category` (`id`) ON DELETE SET NULL ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=109 DEFAULT CHARSET=utf8;
+CREATE TABLE `book`
+(
+    `id`     int(11)      NOT NULL AUTO_INCREMENT,
+    `cover`  varchar(255)          DEFAULT '',
+    `title`  varchar(255) NOT NULL DEFAULT '',
+    `author` varchar(255)          DEFAULT '',
+    `date`   varchar(20)           DEFAULT '',
+    `press`  varchar(255)          DEFAULT '',
+    `abs`    varchar(255)          DEFAULT NULL,
+    `cid`    int(11)               DEFAULT NULL,
+    PRIMARY KEY (`id`),
+    KEY `fk_book_category_on_cid` (`cid`),
+    CONSTRAINT `fk_book_category_on_cid` FOREIGN KEY (`cid`) REFERENCES `category` (`id`) ON DELETE SET NULL ON UPDATE CASCADE
+) ENGINE = InnoDB
+  AUTO_INCREMENT = 109
+  DEFAULT CHARSET = utf8;
 
 -- ----------------------------
 -- Table structure for category
 -- ----------------------------
 DROP TABLE IF EXISTS `category`;
-CREATE TABLE `category` (
-  `id` int(11) NOT NULL,
-  `name` varchar(255) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+CREATE TABLE `category`
+(
+    `id`   int(11)      NOT NULL,
+    `name` varchar(255) NOT NULL,
+    PRIMARY KEY (`id`)
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8;
