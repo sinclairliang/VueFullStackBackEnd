50e8dbcc7afa22927a4f248d8936a841f904aca3
Sinclair Liang <sinclairliang@gmail.com>
2020-08-24 17:16:57 -0500

    new DAOs

---
+++ src/main/java/com/evan/wj/service/AdminMenuService.java
@@ -0,0 +1,9 @@
+package com.evan.wj.service;
+
+import com.evan.wj.dao.AdminMenuDAO;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class AdminMenuService {
+    @Autowired
+    AdminMenuDAO adminMenuDAO;
+}
