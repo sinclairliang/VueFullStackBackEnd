47ac7cae078d17e47ef0989fde88e23895652326
Sinclair Liang <sinclairliang@gmail.com>
2020-08-26 01:11:46 -0500

    admin role menu

--- src/main/java/com/evan/wj/service/AdminRoleMenuService.java
+++ src/main/java/com/evan/wj/service/AdminRoleMenuService.java
@@ -3,9 +3,13 @@ package com.evan.wj.service;
 import com.evan.wj.dao.AdminRoleMenuDAO;
 import com.evan.wj.pojo.AdminRoleMenu;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 @Service
 public class AdminRoleMenuService {
@@ -18,5 +22,23 @@ public class AdminRoleMenuService {
 
     public List<AdminRoleMenu> findAllByRid(List<Integer> rids) {
         return adminRoleMenuDAO.findAllByRid(rids);
+    }
+
+    public void save(AdminRoleMenu roleMenu) {
+        adminRoleMenuDAO.save(roleMenu);
+    }
+
+    @Modifying
+    @Transactional
+    public void updateRoleMenu(int rid, Map<String, List<Integer>> menusIds) {
+        adminRoleMenuDAO.deleteByRid(rid);
+        List<AdminRoleMenu> roleMenus = new ArrayList<>();
+        for (Integer mid : menusIds.get("menusIds")) {
+            AdminRoleMenu roleMenu = new AdminRoleMenu();
+            roleMenu.setMid(mid);
+            roleMenu.setRid(rid);
+            roleMenus.add(roleMenu);
+        }
+        adminRoleMenuDAO.saveAll(roleMenus);
     }
 }
