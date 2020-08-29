73d9585f6a4d9a024e3bf43d31a3ff2a326aa3ee
Sinclair Liang <sinclairliang@gmail.com>
2020-08-28 20:22:46 -0500

    trying new method in controller

--- src/main/java/com/evan/wj/controller/MenuController.java
+++ src/main/java/com/evan/wj/controller/MenuController.java
@@ -1,12 +1,12 @@
 package com.evan.wj.controller;
 
 import com.evan.wj.pojo.AdminMenu;
-import com.evan.wj.result.Result;
-import com.evan.wj.result.ResultFactory;
 import com.evan.wj.service.AdminMenuService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class MenuController {
@@ -14,7 +14,8 @@ public class MenuController {
     AdminMenuService adminMenuService;
 
     @GetMapping("/api/menu")
-    public Result menu() {
-        return ResultFactory.buildSuccessResult(adminMenuService.getMenusByCurrentUser());
+    public List<AdminMenu> menu() {
+        return adminMenuService.getMenusByCurrentUser();
     }
+
 }
