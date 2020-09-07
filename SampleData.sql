43d54aabee443b1043c1a3bd560ccdb7dabee5f6
Sinclair Liang <sinclairliang@gmail.com>
2020-09-07 02:56:22 -0500

    added menu items

---
+++ SampleData.sql
@@ -0,0 +1,13 @@
+-- ----------------------------
+-- Records of admin_menu
+-- ----------------------------
+INSERT INTO `admin_menu` VALUES ('1', '/admin', 'AdminIndex', 'el-icon-s-home', 'AdminIndex', '0');
+INSERT INTO `admin_menu` VALUES ('2', '/admin/dashboard', 'DashboardAdmin', null, 'dashboard/admin/index', '1');
+INSERT INTO `admin_menu` VALUES ('3', '/admin', 'User', 'el-icon-user', 'AdminIndex', '0');
+INSERT INTO `admin_menu` VALUES ('4', '/admin', 'Content', 'el-icon-tickets', 'AdminIndex', '0');
+INSERT INTO `admin_menu` VALUES ('5', '/admin', 'System', 'el-icon-s-tools', 'AdminIndex', '0');
+INSERT INTO `admin_menu` VALUES ('6', '/admin/user/profile', 'Profile', null, 'user/UserProfile', '3');
+INSERT INTO `admin_menu` VALUES ('7', '/admin/user/role', 'Role', null, 'user/Role', '3');
+INSERT INTO `admin_menu` VALUES ('8', '/admin/content/book', 'BookManagement', null, 'content/BookManagement', '4');
+INSERT INTO `admin_menu` VALUES ('9', '/admin/content/banner', 'BannerManagement', null, 'content/BannerManagement', '4');
+INSERT INTO `admin_menu` VALUES ('10', '/admin/content/article', 'ArticleManagement', null, 'content/ArticleManagement', '4');
