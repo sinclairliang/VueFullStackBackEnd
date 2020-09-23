95977c5ff5817da3ec92ca2f12291246cd3ba846
Sinclair Liang <sinclairliang@gmail.com>
2020-09-22 22:03:58 -0500

    Added:     - DefaultExceptionHandler code

--- src/main/java/com/evan/wj/exception/DefaultExceptionHandler.java
+++ src/main/java/com/evan/wj/exception/DefaultExceptionHandler.java
@@ -1,4 +1,34 @@
 package com.evan.wj.exception;
 
+import com.evan.wj.result.Result;
+import com.evan.wj.result.ResultFactory;
+import org.apache.shiro.authz.UnauthorizedException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@ControllerAdvice
+@ResponseBody
 public class DefaultExceptionHandler {
+    @ExceptionHandler(value = Exception.class)
+    public Result exceptionHandler(Exception e) {
+        String message = null;
+
+        if (e instanceof IllegalArgumentException) {
+            message = "Illegal Arguments";
+        }
+
+        if (e instanceof MethodArgumentNotValidException) {
+            message = ((MethodArgumentNotValidException) e)
+                    .getBindingResult()
+                    .getFieldError()
+                    .getDefaultMessage();
+        }
+
+        if (e instanceof UnauthorizedException) {
+            message = "Unauthorized Exception";
+        }
+        return ResultFactory.buildFailResult(message);
+    }
 }
